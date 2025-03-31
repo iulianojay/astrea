abe9f07e46719567670fa5aaf1b73b03d8a6482c
Trent <trentvoris@gmail.com>
2025-03-30 18:45:46 -0700

    Added Avro and had to change the name of constinit to constinit_

---
+++ newtool/vehicles/vehicles/testcpp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <avro/Schema.hh>
+
+int main() {
+    avro::ValidSchema schema;
+    std::cout << "Avro C++ library is successfully installed!" << std::endl;
+    return 0;
+}
