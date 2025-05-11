55a995f27c7039af68fa67793e6faa65ede0e76a
Jay Iuliano <iuliano.jay@gmail.com>
2025-05-11 15:56:35 -0600

    WVGD-44: Cleaned up local avro install. Made separate subfolder for databasing "snapshot". Got first example compiled and running

--- newtool/vehicles/vehicles/testcpp.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <avro/Schema.hh>
-
-int main() {
-    avro::ValidSchema schema;
-    std::cout << "Avro C++ library is successfully installed!" << std::endl;
-    return 0;
-}
