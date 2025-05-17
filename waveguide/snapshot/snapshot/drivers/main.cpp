21fcccae3ef7fc878e6a0317d627cade9e4f8876
Jay Iuliano <iuliano.jay@gmail.com>
2025-05-17 10:49:53 -0600

    WVGD-44: Added basic example of GET query to thz satdb

--- waveguide/snapshot/snapshot/drivers/main.cpp
+++ waveguide/snapshot/snapshot/drivers/main.cpp
@@ -1,124 +1,26 @@
-#include <fstream>
-#include <memory>
+#include <iostream>
+#include <string>
 
-#include <avro/Compiler.hh>
-#include <avro/DataFile.hh>
-#include <avro/Decoder.hh>
-#include <avro/Encoder.hh>
-#include <avro/Generic.hh>
-#include <avro/Specific.hh>
-#include <avro/Stream.hh>
-#include <avro/ValidSchema.hh>
-
-#include <astro/astro.hpp>
-
-using namespace astro;
-using namespace mp_units;
-using mp_units::si::unit_symbols::km;
-using mp_units::si::unit_symbols::s;
-
-void ex1();
-void ex2();
-void ex3();
+#include <cpr/cpr.h>
 
 int main()
 {
-    // ex1();
-    // ex2();
-    ex3();
+    cpr::Url url             = "https://satdb.ethz.ch/api/satellitedata";
+    cpr::Authentication auth = { "user", "pass", cpr::AuthMode::BASIC };
+    cpr::Parameters params   = {
+          { "start-datetime", "20230409T0000" }, // start date YYYYMMDDThhmm
+          { "end-datetime", "20230410T0000" },   // end date
+          { "before", "3" },                     // days before
+          { "after", "3" },                      // days after
+          { "norad-id", "25544" },               // exact norad id
+          { "without-frequency-data", "True" },  // include frequency data
+                                                 // { "frequency-list", "[10.7-12.7,13.85-14.5]" },
+    };
 
+    cpr::Response r = cpr::Get(url, auth, params);
+
+    std::cout << r.status_code << std::endl;
+    std::cout << r.header["content-type"] << std::endl;
+    std::cout << r.text << std::endl;
     return 0;
-}
-
-void ex1()
-{
-    // Build an output stream and an encoder
-    avro::OutputStreamPtr outStream = avro::memoryOutputStream();
-    avro::EncoderPtr encoder        = avro::binaryEncoder();
-    encoder->init(*outStream);
-
-    // Build out structure
-    Cartesian c1(10000 * km, 0.0 * km, 0.0 * km, 10.0 * km / s, 0.0 * km / s, 0.0 * km / s);
-
-    // Encode
-    avro::encode(*encoder, c1);
-
-    // Build an input stream and a decoder
-    avro::InputStreamPtr inStream = avro::memoryInputStream(*outStream);
-    avro::DecoderPtr decoder      = avro::binaryDecoder();
-    decoder->init(*inStream);
-
-    // Decode into new structure
-    Cartesian c2;
-    avro::decode(*decoder, c2);
-
-    // Print
-    std::cout << c2 << std::endl;
-}
-
-
-void ex2()
-{
-    std::ifstream infileStream("/home/jay/projects/waveguide/waveguide/snapshot/data/cartesian.json");
-
-    avro::ValidSchema cartesianSchema;
-    avro::compileJsonSchema(infileStream, cartesianSchema);
-
-    // Build an output stream and an encoder
-    avro::OutputStreamPtr outStream = avro::memoryOutputStream();
-    avro::EncoderPtr encoder        = avro::validatingEncoder(cartesianSchema, avro::binaryEncoder());
-    encoder->init(*outStream);
-
-    // Build out structure
-    Cartesian c1(10000 * km, 0.0 * km, 0.0 * km, 10.0 * km / s, 0.0 * km / s, 0.0 * km / s);
-
-    // Encode
-    avro::encode(*encoder, c1);
-
-    // Build an input stream and a decoder
-    avro::InputStreamPtr inStream = avro::memoryInputStream(*outStream);
-    avro::DecoderPtr decoder      = avro::validatingDecoder(cartesianSchema, avro::binaryDecoder());
-    decoder->init(*inStream);
-
-    // Decode into new structure
-    Cartesian c2;
-    avro::decode(*decoder, c2);
-
-    // Print
-    std::cout << c2 << std::endl;
-}
-
-avro::ValidSchema loadSchema(const std::string& filename);
-
-avro::ValidSchema loadSchema(const std::string& filename)
-{
-    std::ifstream ifs(filename);
-    avro::ValidSchema result;
-    avro::compileJsonSchema(ifs, result);
-    return result;
-}
-
-void ex3()
-{
-    // Load schema
-    std::string schemaFile = "./waveguide/snapshot/data/cartesian.json";
-    std::string outFile    = "./waveguide/snapshot/data/cartesian.avro";
-
-    avro::ValidSchema cartesianSchema = loadSchema(schemaFile);
-
-    // Write to file
-    avro::DataFileWriter<Cartesian> dataFileWriter(outFile.c_str(), cartesianSchema);
-    Cartesian c1;
-    for (double i = 0.0; i < 100.0; i++) {
-        c1 = Cartesian((10000.0 + i) * km, i * km, i * km, (1.0 + i / 100.0) * km / s, i / 100.0 * km / s, i / 100.0 * km / s);
-        dataFileWriter.write(c1);
-    }
-    dataFileWriter.close();
-
-    // Read from file
-    avro::DataFileReader<Cartesian> dataFileReader(outFile.c_str(), cartesianSchema);
-    Cartesian c2;
-    while (dataFileReader.read(c2)) {
-        std::cout << c2 << std::endl;
-    }
 }
