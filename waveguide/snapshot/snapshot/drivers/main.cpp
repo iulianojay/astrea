419371035f358b96912a14eba79bdf9d81ccb775
Jay Iuliano <iuliano.jay@gmail.com>
2025-05-17 15:44:28 -0600

    WVGD-44: Added Quierier for ETHZ database (pulled from Celestrack). Added several utilites to Date, fixed minor inconsistencies

--- waveguide/snapshot/snapshot/drivers/main.cpp
+++ waveguide/snapshot/snapshot/drivers/main.cpp
@@ -1,26 +1,26 @@
+#include <iomanip>
 #include <iostream>
 #include <string>
 
-#include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
+
+#include <astro/time/Date.hpp>
+
+#include <snapshot/http-queries/EthzQuerier.hpp>
+
+using namespace snapshot;
 
 int main()
 {
-    cpr::Url url             = "https://satdb.ethz.ch/api/satellitedata";
-    cpr::Authentication auth = { "user", "pass", cpr::AuthMode::BASIC };
-    cpr::Parameters params   = {
-          { "start-datetime", "20230409T0000" }, // start date YYYYMMDDThhmm
-          { "end-datetime", "20230410T0000" },   // end date
-          { "before", "3" },                     // days before
-          { "after", "3" },                      // days after
-          { "norad-id", "25544" },               // exact norad id
-          { "without-frequency-data", "True" },  // include frequency data
-                                                 // { "frequency-list", "[10.7-12.7,13.85-14.5]" },
-    };
+    const astro::Date startDate("2024-01-01 00:00:00.0");
+    const astro::Date endDate("2024-01-02 00:00:00.0");
+    // const unsigned noradId = 44714;
+    const std::string name = "NAVSTAR";
 
-    cpr::Response r = cpr::Get(url, auth, params);
+    EthzQuerier ethz;
 
-    std::cout << r.status_code << std::endl;
-    std::cout << r.header["content-type"] << std::endl;
-    std::cout << r.text << std::endl;
+    // std::cout << std::endl << std::setw(4) << ethz.query(startDate, endDate, noradId) << std::endl;
+    std::cout << std::endl << std::setw(4) << ethz.query(startDate, endDate, name, true) << std::endl;
+
     return 0;
 }
---
+++ waveguide/snapshot/snapshot/http-queries/EthzQuerier.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
+
+#include <astro/time/Date.hpp>
+
+namespace snapshot {
+
+class EthzQuerier {
+  public:
+    EthzQuerier() :
+        _minStartDate("2023-03-10 00:00:00.0"){};
+    ~EthzQuerier() = default;
+
+    nlohmann::json query(const astro::Date& startDate, const astro::Date& endDate, const std::string& name, const bool& includeFrequencyData = false);
+    nlohmann::json query(const astro::Date& startDate, const astro::Date& endDate, const unsigned& noradId, const bool& includeFrequencyData = false);
+
+  private:
+    const cpr::Url _base               = "https://satdb.ethz.ch/api";
+    const cpr::Url _satDataUrl         = _base + "/satellitedata";
+    const cpr::Url _satUrl             = _base + "/satellite";
+    const cpr::Url _frequencySourceUrl = _base + "/frequencysource";
+    const cpr::Url _frequencyUrl       = _base + "/frequency";
+    const cpr::Authentication _auth    = { "user", "pass", cpr::AuthMode::BASIC };
+
+    const astro::Date _minStartDate;
+
+    nlohmann::json query_impl(
+        const astro::Date& startDate,
+        const astro::Date& endDate,
+        const std::string& name          = "",
+        const unsigned& noradId          = 0,
+        const bool& includeFrequencyData = false
+    );
+    void clean_result(nlohmann::json& result);
+};
+
+} // namespace snapshot
