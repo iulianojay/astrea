bd0481afb52f51061d5c58e95eba3a27d2f161e8
Jay Iuliano <iuliano.jay@gmail.com>
2026-03-14 20:46:08 +0200

    Improvement/27 64 event cleanup (#92) #minor
    * #27 Updated impulsive burn to be more generic and work under several common trigger conditions
    
    * #64 Added deorbit event
    
    * #27/#64 Added tests for new events
    
    * #27/#64 Fixed event example so it still shows a nice period increase after each burn

---
+++ astrea/astro/astro/propagation/event_detection/events/Deorbit.cpp
@@ -0,0 +1,45 @@
+/*
+ * The GNU Lesser General Public License (LGPL)
+ *
+ * Copyright (c) 2026 Jay Iuliano
+ *
+ * This file is part of Astrea.
+ * Astrea is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * Astrea is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should
+ * have received a copy of the GNU General Public License along with Astrea. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <astro/propagation/event_detection/events/Deorbit.hpp>
+
+#include <astro/state/State.hpp>
+#include <astro/state/angular_elements/instances/Geodetic.hpp>
+
+using mp_units::si::unit_symbols::km;
+
+namespace astrea {
+namespace astro {
+
+Deorbit::Deorbit(const Distance& triggerAltitude) :
+    _triggerAltitude(triggerAltitude)
+{
+}
+
+std::string Deorbit::get_name() const { return "Deorbit"; }
+
+Unitless Deorbit::measure_event(const Time& time, const State& state, const Vehicle& vehicle) const
+{
+    const Cartesian elements = state.in_element_set<Cartesian>();
+
+    const CelestialBodyUniquePtr& center = state.get_system().get_central_body();
+    const Distance altitude = Geodetic(elements.get_position(), state.get_epoch(), center.get()).get_altitude();
+
+    if (_triggerAltitude != 0.0 * km) { return (altitude - _triggerAltitude) / (1.0 * km); }
+    return (altitude - center->get_crash_radius()) / (1.0 * km);
+}
+
+bool Deorbit::is_terminal() const { return true; }
+
+} // namespace astro
+} // namespace astrea
