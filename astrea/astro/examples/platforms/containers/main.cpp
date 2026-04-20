2c7fc7817efe735d9c6de5c4b314ceab3bcfc8d2
Jay Iuliano <jay.iuliano@iceye.com>
2026-04-20 08:01:31 +0000

    #111 - Updated container and payload examples

--- astrea/astro/examples/platforms/containers/main.cpp
+++ astrea/astro/examples/platforms/containers/main.cpp
@@ -21,4 +21,34 @@ using namespace astrea;
 using namespace astro;
 using namespace mp_units;
 
-int main() { return 0; }
+using mp_units::angular::unit_symbols::deg;
+using mp_units::si::unit_symbols::km;
+
+int main()
+{
+    // Astrea comes with a few simple containers for Vehicles. These are meant for quick organization and could be
+    // quickly replaced with custom alternatives for more specific use cases.
+
+    // Build a Spacecraft Constellation with Walker parameters
+    AstrodynamicsSystem sys;
+    Date epoch("2024-01-01T00:00:00Z");
+    const Distance sma  = 7000.0 * km;
+    const Angle inc     = 45.0 * deg;
+    const std::size_t T = 100; // Total number of spacecraft
+    const std::size_t P = 5;   // Number of planes
+    const double F      = 1.0; // Phasing parameters
+    Constellation<Spacecraft> constellation(sys, epoch, sma, inc, T, P, F);
+
+    // Iterate over the constellation and print the initial state of each spacecraft
+    for (const auto& shell : constellation.get_shells()) {
+        std::cout << "Shell ID: " << shell.get_id() << std::endl;
+        for (const auto& plane : shell.get_planes()) {
+            std::cout << "\tPlane ID: " << plane.get_id() << std::endl;
+            for (const auto& satellite : plane.get_all_spacecraft()) {
+                std::cout << "\t\tSatellite ID: " << satellite.get_id() << std::endl;
+                std::cout << "\t\tInitial State: " << satellite.get_initial_state() << std::endl;
+            }
+        }
+    }
+    return 0;
+}
--- astrea/astro/examples/platforms/payloads/main.cpp
+++ astrea/astro/examples/platforms/payloads/main.cpp
@@ -21,4 +21,78 @@ using namespace astrea;
 using namespace astro;
 using namespace mp_units;
 
-int main() { return 0; }
+using mp_units::si::unit_symbols::kg;
+using mp_units::si::unit_symbols::km;
+using mp_units::si::unit_symbols::s;
+
+// Astrea provides an interface for generically defining Payloads. The goal
+// of the payload design is to allow users to define and attach objects to a
+// spacecraft, while being flexible and extensible, and keeping boilerplate code
+// to a minimum.
+
+// First we define a payload and it's parameters
+struct MyPayloadParameters : public PayloadParameters {
+    MyPayloadParameters(const Mass& mass) :
+        PayloadParameters(),
+        _mass(mass)
+    {
+    }
+    Mass get_mass() const { return _mass; }
+
+  private:
+    Mass _mass;
+};
+
+// The payload uses CRTP to generate an "interface" that provides more generic container-like methods for the platform.
+// This comes with the drawbacks of nasty pointers, lifetime issues, structural requirements for parent objects, and a
+// wash of other annoying things. For now, it works, but it will likely be redesigned in the future.
+struct MyPayload : public Payload<MyPayload, MyPayloadParameters> {
+    template <typename Parent_T>
+        requires(std::is_base_of_v<PayloadPlatform<MyPayload>, Parent_T>)
+    MyPayload(const Parent_T& parent, const MyPayloadParameters& parameters) :
+        Payload(parent, parameters),
+        _id(utilities::IdProvider::get_next_id<"MyPayload">())
+    {
+    }
+
+    std::size_t get_id() const { return _id; }
+
+    Mass get_mass() const { return get_parameters().get_mass(); }
+
+  private:
+    std::size_t _id;
+};
+
+// Finally, we use the PayloadPlatform to attach our paylaod to a platform. Payload platforms are expected to be a
+// FrameReference as well so we need to implement those methods. This will likely change in the future.
+struct MySpacecraft : public PayloadPlatform<MyPayload> {
+    std::size_t get_id() const { return 0; }
+    std::string get_name() const { return "MySpacecraft"; }
+    CartesianVector<Distance, frames::earth::icrf> get_inertial_position(const Date& date) const
+    {
+        return { 0.0 * km, 0.0 * km, 0.0 * km };
+    }
+    CartesianVector<Velocity, frames::earth::icrf> get_inertial_velocity(const Date& date) const
+    {
+        return { 0.0 * km / s, 0.0 * km / s, 0.0 * km / s };
+    }
+};
+
+int main()
+{
+    // Now we can build our spacecraft and attach as many payloads as we like
+    MySpacecraft spacecraft;
+    spacecraft.attach_payload({ 100.0 * kg });
+    spacecraft.attach_payload({ 200.0 * kg });
+    const auto payloads = spacecraft.get_payloads();
+    for (const auto& payload : payloads) {
+        std::cout << "Payload ID: " << payload.get_id() << ", Mass: " << payload.get_mass() << std::endl;
+    }
+    return 0;
+}
+
+// The goals was to avoid writing dozens of boilerplate classes awash with methods such
+// as get/set_thruster, get/set_reaction_wheel, get/set_sensor, etc. While this design works, it's currently
+// overdone and needlessly fragile. By making it generic and compatible with multiple inheritance, the supporting
+// code is hard to work with and extensibility is technically possible but difficult. This structure also currently
+// depends on CRTP, which can cause issues on some compilers.
